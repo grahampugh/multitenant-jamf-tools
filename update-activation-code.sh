1e7c2c02db34278f4eb54d830e782362e22d94b4
Graham Pugh <graham.pugh@jamf.com>
2024-09-27 13:10:49 +0200

    add slack webhook, add are_you_sure

--- update-activation-code.sh
+++ update-activation-code.sh
@@ -30,6 +30,59 @@ Usage:
 USAGE
 }
 
+are_you_sure() {
+    if [[ $confirmed != "yes" ]]; then
+        echo
+        read -r -p "Are you sure you want to update the activation code on $jss_instance? (Y/N) : " sure
+        case "$sure" in
+            Y|y)
+                return
+                ;;
+            *)
+                echo "   [are_you_sure] Action cancelled, quitting"
+                exit
+                ;;
+        esac
+    fi
+}
+
+send_slack_notification() {
+    local jss_url="$1"
+    local acode="$2"
+
+    if get_slack_webhook "$instance_list_file"; then
+        if [[ $slack_webhook_url ]]; then
+            slack_text="{'username': '$jss_url', 'text': 'Instance: $jss_url\nActivation Code Update: $acode'}"
+        
+            response=$(
+                curl -s -o /dev/null -S -i -X POST -H "Content-Type: application/json" \
+                --write-out '%{http_code}' \
+                --data "$slack_text" \
+                "$slack_webhook_url"
+            )
+            echo "   [send_slack_notification] Sent Slack notification (response: $response)"
+        fi
+    else
+        echo "   [send_slack_notification] Not sending slack notification as no webhook found"
+    fi
+}
+
+get_activation_code() {
+    # get the original activation code for outputting
+    # determine jss_url
+    set_credentials "$jss_instance"
+    jss_url="${jss_instance}"
+    # send request
+    curl_url="$jss_url/JSSResource/activationcode"
+    curl_args=("--request")
+    curl_args+=("GET")
+    curl_args+=("--header")
+    curl_args+=("Accept: application/json")
+    send_curl_request
+
+    old_activation_code=$(plutil -extract "activation_code.code" raw "$curl_output_file")
+}
+
 write_activation_code() {
     # determine jss_url
     set_credentials "$jss_instance"
@@ -43,6 +96,9 @@ write_activation_code() {
     curl_args+=("--data")
     curl_args+=("<activation_code><code>$activation_code</code></activation_code>")
     send_curl_request
+
+    # Send Slack notification
+    send_slack_notification "$jss_instance" "$activation_code"
 }
 
 if [[ ! -d "${this_script_dir}" ]]; then
@@ -75,6 +131,10 @@ while [[ "$#" -gt 0 ]]; do
         -a|--all)
             all_instances=1
         ;;
+        --confirm)
+            echo "   [main] CLI: Action: auto-confirm copy or delete, for non-interactive use."
+            confirmed="yes"
+        ;;
         -v|--verbose)
             verbose=1
         ;;
@@ -104,17 +164,20 @@ if [[ ! $activation_code  ]]; then
 fi
 
 # get specific instance if entered
-if [[ $chosen_instance ]]; then
-    jss_instance="$chosen_instance"
-    echo "Updating Activation Code on $jss_instance..."
-    write_activation_code
-else
-    for instance in "${instances_list[@]}"; do
-        jss_instance="$instance"
+for instance in "${instance_choice_array[@]}"; do
+    jss_instance="$instance"
+    get_activation_code
+    echo "Existing Activation Code on $jss_instance: $old_activation_code"
+    if [[ "$old_activation_code" == "$activation_code" ]]; then
+        echo "Existing Activation Code on $jss_instance matches the inputted code. No need to update."
+    else
+        # are we sure to proceed?
+        are_you_sure
+
         echo "Updating Activation Code on $jss_instance..."
         write_activation_code
-    done
-fi
+    fi
+done
 
 echo 
 echo "Finished"
