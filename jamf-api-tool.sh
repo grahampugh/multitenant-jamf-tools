957b1b195d74538e72b920d97c88ff374e378a29
Graham Pugh <graham.pugh@jamf.com>
2025-09-10 19:38:39 +0100

    interactive menus and confirm to delete searched policies

--- jamf-api-tool.sh
+++ jamf-api-tool.sh
@@ -99,8 +99,10 @@ confirm() {
             echo
             echo "Please confirm that you would like to perform the following command"
             echo "on instance $jss_instance:"
-            echo "jamf-api-tool.py ${args[*]}"
-            read -r -p "(Y/N) : " are_you_sure
+            echo
+            echo "   jamf-api-tool.py ${args[*]}"
+            echo
+            read -r -p "Enter Y/N : " are_you_sure
             case "$are_you_sure" in
                 Y|y)
                     echo "   [main] Confirmed"
@@ -162,6 +164,12 @@ while test $# -gt 0 ; do
         --confirm)
             confirmed="yes"
             ;;
+        --tool-dir)
+            shift
+            tool_directory="$1"
+            # if ~ is supplied in the path, expand it
+            tool_directory="${tool_directory/#\~/$HOME}"
+            ;;
         -v*)
             args+=("$1")
             ;;
@@ -189,7 +197,342 @@ echo
 /bin/mkdir -p "$output_dir"
 
 # ------------------------------------------------------------------------------------
-# 1. Ask for the instance list, show list, ask to apply to one, multiple or all
+# Ask for the object type if not supplied in the args
+# Valid args are:
+# --computers
+# --packages
+# --policies
+# --scripts
+# --ea
+# --groups / --macosgroups
+# --iosgroups
+# --macosprofiles
+# --iosprofiles
+# --acs
+# --ads
+# ------------------------------------------------------------------------------------
+
+if [[ ! "${args[*]}" == *"--computers"* ]] && \
+   [[ ! "${args[*]}" == *"--packages"* ]] && \
+   [[ ! "${args[*]}" == *"--policies"* ]] && \
+   [[ ! "${args[*]}" == *"--scripts"* ]] && \
+   [[ ! "${args[*]}" == *"--ea"* ]] && \
+   [[ ! "${args[*]}" == *"--groups"* ]] && \
+   [[ ! "${args[*]}" == *"--macosgroups"* ]] && \
+   [[ ! "${args[*]}" == *"--iosgroups"* ]] && \
+   [[ ! "${args[*]}" == *"--macosprofiles"* ]] && \
+   [[ ! "${args[*]}" == *"--iosprofiles"* ]] && \
+   [[ ! "${args[*]}" == *"--acs"* ]] && \
+   [[ ! "${args[*]}" == *"--ads"* ]]; then
+    echo "Please choose the object type to be processed:"
+    echo "1) Computers"
+    echo "2) Packages"
+    echo "3) Policies"
+    echo "4) Scripts"
+    echo "5) Extension Attributes"
+    echo "6) Groups (Mac)"
+    echo "7) Groups (iOS)"
+    echo "8) Configuration Profiles (Mac)"
+    echo "9) Configuration Profiles (iOS)"
+    echo "10) Advanced Searches (Mac)"
+    echo "11) Advanced Searches (iOS)"
+    read -r -p "(1-11): " object_type
+    case "$object_type" in
+        1)
+            args+=("--computers")
+            ;;
+        2)
+            args+=("--packages")
+            ;;
+        3)
+            args+=("--policies")
+            ;;
+        4)
+            args+=("--scripts")
+            ;;
+        5)
+            args+=("--ea")
+            ;;
+        6)
+            args+=("--macosgroups")
+            ;;
+        7)
+            args+=("--iosgroups")
+            ;;
+        8)
+            args+=("--macosprofiles")
+            ;;
+        9)
+            args+=("--iosprofiles")
+            ;;
+        10)
+            args+=("--acs")
+            ;;
+        11)
+            args+=("--ads")
+            ;;
+        *)
+            echo "Invalid choice. Exiting."
+            exit 1
+            ;;
+    esac
+    echo
+fi
+
+# ------------------------------------------------------------------------------------
+# If packages, and valid options not supplied at the command line,
+# ask which option is needed
+# ------------------------------------------------------------------------------------
+
+if [[ "${args[*]}" == *"--packages"* ]]; then
+    if [[ ! "${args[*]}" == *"--all"* ]] && \
+       [[ ! "${args[*]}" == *"--unused"* ]] && \
+       [[ ! "${args[*]}" == *"--search"* ]]; then
+        echo "Please choose the action to be performed on Packages:"
+        echo "1) List all packages"
+        echo "2) List all packages with additional details"
+        echo "3) List unused packages"
+        echo "4) List and delete unused packages"
+        echo "5) Search packages by name"
+        echo "6) Search and delete packages by name"
+        read -r -p "(1-6): " action
+        case "$action" in
+            1)
+                args+=("--all")
+                if [[ "${args[*]}" == *"--details"* ]]; then
+                    echo "   [main] Warning: --details option supplied at command line will be ignored"
+                    args=("${args[@]/--details}")
+                fi
+                ;;
+            2)
+                args+=("--all")
+                if [[ ! "${args[*]}" == *"--details"* ]]; then
+                    args+=("--details")
+                fi
+                ;;
+            3)
+                args+=("--unused")
+                if [[ "${args[*]}" == *"--delete"* ]]; then
+                    echo "   [main] Warning: --delete option supplied at command line will be ignored"
+                    args=("${args[@]/--delete}")
+                fi
+                ;;
+            4)
+                args+=("--unused")
+                if [[ ! "${args[*]}" == *"--delete"* ]]; then
+                    args+=("--delete")
+                fi
+                ;;
+            5)
+                args+=("--search")
+                if [[ "${args[*]}" == *"--delete"* ]]; then
+                    echo "   [main] Warning: --delete option supplied at command line will be ignored"
+                    args=("${args[@]/--delete}")
+                fi
+                ;;
+            6)
+                args+=("--search")
+                read -r -p "Enter search string: " search_string
+                args+=("$search_string")
+                if [[ ! "${args[*]}" == *"--delete"* ]]; then
+                    args+=("--delete")
+                fi
+                ;;
+            *)
+                echo "Invalid choice. Exiting."
+                exit 1
+                ;;
+        esac
+        echo
+    fi
+elif [[ "${args[*]}" == *"--policies"* ]]; then
+    if [[ ! "${args[*]}" == *"--all"* ]] && \
+       [[ ! "${args[*]}" == *"--unused"* ]] && \
+       [[ ! "${args[*]}" == *"--search"* ]] && \
+       [[ ! "${args[*]}" == *"--category"* ]] && \
+       [[ ! "${args[*]}" == *"--from_csv"* ]]; then
+        echo "Please choose the action to be performed on Policies:"
+        echo "1) List all policies"
+        echo "2) List disabled policies"
+        echo "3) List and delete disabled policies"
+        echo "4) List unused policies"
+        echo "5) List and delete unused policies"
+        echo "6) List policies in a defined category"
+        echo "7) List and delete policies in a defined category"
+        echo "8) Search policies by name"
+        echo "9) Search and delete policies by name"
+        echo "10) Delete policies from a CSV"
+        read -r -p "(1-10): " action
+        case "$action" in
+            1)
+                args+=("--all")
+                if [[ "${args[*]}" == *"--details"* ]]; then
+                    echo "   [main] Warning: --details option supplied at command line will be ignored"
+                    args=("${args[@]/--details}")
+                fi
+                ;;
+            2)
+                args+=("--disabled")
+                if [[ "${args[*]}" == *"--delete"* ]]; then
+                    echo "   [main] Warning: --delete option supplied at command line will be ignored"
+                    args=("${args[@]/--delete}")
+                fi
+                ;;
+            3)
+                args+=("--disabled")
+                if [[ ! "${args[*]}" == *"--delete"* ]]; then
+                    args+=("--delete")
+                fi
+                ;;
+            4)
+                args+=("--unused")
+                if [[ "${args[*]}" == *"--delete"* ]]; then
+                    echo "   [main] Warning: --delete option supplied at command line will be ignored"
+                    args=("${args[@]/--delete}")
+                fi
+                ;;
+            5)
+                args+=("--unused")
+                if [[ ! "${args[*]}" == *"--delete"* ]]; then
+                    args+=("--delete")
+                fi
+                ;;
+            6)
+                args+=("--category")
+                read -r -p "Enter category name: " category_name
+                args+=("$category_name")
+                if [[ "${args[*]}" == *"--delete"* ]]; then
+                    echo "   [main] Warning: --delete option supplied at command line will be ignored"
+                    args=("${args[@]/--delete}")
+                fi
+                ;;
+            7)
+                args+=("--category")
+                read -r -p "Enter category name: " category_name
+                args+=("$category_name")
+                if [[ ! "${args[*]}" == *"--delete"* ]]; then
+                    args+=("--delete")
+                fi
+                ;;
+            8)
+                args+=("--search")
+                read -r -p "Enter search string: " search_string
+                args+=("$search_string")
+                if [[ "${args[*]}" == *"--delete"* ]]; then
+                    echo "   [main] Warning: --delete option supplied at command line will be ignored"
+                    args=("${args[@]/--delete}")
+                fi
+                ;;
+            9)
+                args+=("--search")
+                read -r -p "Enter search string: " search_string
+                args+=("$search_string")
+                if [[ ! "${args[*]}" == *"--delete"* ]]; then
+                    args+=("--delete")
+                fi
+                ;;
+            10)
+                args+=("--from_csv")
+                read -r -p "Enter the full path to the CSV file: " csv_path
+                args+=("$csv_path")
+                if [[ ! -f "$csv_path" ]]; then
+                    echo "   [main] Error: $csv_path not found. Exiting."
+                    exit 1
+                fi
+                if [[ ! "${args[*]}" == *"--delete"* ]]; then
+                    args+=("--delete")
+                fi
+                ;;
+            *)
+                echo "Invalid choice. Exiting."
+                exit 1
+                ;;
+        esac
+        echo
+    fi
+elif [[ "${args[*]}" == *"--macosprofiles"* || "${args[*]}" == *"--iosprofiles"* || "${args[*]}" == *"--scripts"* || "${args[*]}" == *"--ea"* || "${args[*]}" == *"--groups"* || "${args[*]}" == *"--macosgroups"* || "${args[*]}" == *"--iosgroups"* || "${args[*]}" == *"--acs"* || "${args[*]}" == *"--ads"* ]]; then
+    if [[ ! "${args[*]}" == *"--all"* ]] && \
+       [[ ! "${args[*]}" == *"--unused"* ]]; then
+        echo "Please choose the action to be performed on objects:"
+        echo "1) List all objects"
+        echo "2) List all objects with additional details"
+        echo "3) List unused objects"
+        echo "4) List and delete unused objects"
+        read -r -p "(1-4): " action
+        case "$action" in
+            1)
+                args+=("--all")
+                if [[ "${args[*]}" == *"--details"* ]]; then
+                    echo "   [main] Warning: --details option supplied at command line will be ignored"
+                    args=("${args[@]/--details}")
+                fi
+                ;;
+            2)
+                args+=("--all")
+                if [[ ! "${args[*]}" == *"--details"* ]]; then
+                    args+=("--details")
+                fi
+                ;;
+            3)
+                args+=("--unused")
+                if [[ "${args[*]}" == *"--delete"* ]]; then
+                    echo "   [main] Warning: --delete option supplied at command line will be ignored"
+                    args=("${args[@]/--delete}")
+                fi
+                ;;
+            4)
+                args+=("--unused")
+                if [[ ! "${args[*]}" == *"--delete"* ]]; then
+                    args+=("--delete")
+                fi
+                ;;
+            *)
+                echo "Invalid choice. Exiting."
+                exit 1
+                ;;
+        esac
+        echo
+    fi
+elif [[ "${args[*]}" == *"--computers"* ]]; then
+    if [[ ! "${args[*]}" == *"--all"* ]] && \
+       [[ ! "${args[*]}" == *"--search"* ]]; then
+        echo "Please choose the action to be performed on Computers:"
+        echo "1) List all computers"
+        echo "2) List all computers with compliance details"
+        echo "3) Search computers by name"
+        read -r -p "(1-3): " action
+        case "$action" in
+            1)
+                args+=("--all")
+                if [[ "${args[*]}" == *"--details"* ]]; then
+                    echo "   [main] Warning: --details option supplied at command line will be ignored"
+                    args=("${args[@]/--details}")
+                fi
+                ;;
+            2)
+                args+=("--all")
+                if [[ ! "${args[*]}" == *"--os"* ]]; then
+                    args+=("--os")
+                    read -r -p "Enter minimum macOS version: " min_os
+                    args+=("$min_os")
+                fi
+                ;;
+            3)
+                args+=("--search")
+                read -r -p "Enter search string: " search_string
+                args+=("$search_string")
+                ;;
+            *)
+                echo "Invalid choice. Exiting."
+                exit 1
+                ;;
+        esac
+        echo
+    fi
+fi
+
+# ------------------------------------------------------------------------------------
+# Ask for the instance list, show list, ask to apply to one, multiple or all
 # ------------------------------------------------------------------------------------
 
 # select the instances that will be changed
@@ -207,14 +550,20 @@ fi
 # get specific instance if entered
 if [[ $chosen_instance ]]; then
     jss_instance="$chosen_instance"
-    confirm
+    # confirm if --delete in the args
+    if [[ "${args[*]}" == *"--delete"* ]]; then
+        confirm
+    fi
     set_credentials "$jss_instance"
     echo "Running jamf-api-tool.py ${args[*]} on $jss_instance..."
     run_api_tool
 else
     for instance in "${instance_choice_array[@]}"; do
         jss_instance="$instance"
-        confirm
+        # confirm if --delete in the args
+        if [[ "${args[*]}" == *"--delete"* ]]; then
+            confirm
+        fi
         set_credentials "$jss_instance"
         echo "Running jamf-api-tool.py ${args[*]} on $jss_instance..."
         run_api_tool
